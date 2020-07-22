0e485d2bbec94cbbda50c301a38bb93ce4557bdf
denis koveshnikov <chiappa@raa-st.com>
2020-07-22 20:04:55 +0300

    - simple sky shader

--- src/4.advanced_opengl/5.1.framebuffers/sky.fs
+++ src/4.advanced_opengl/5.1.framebuffers/sky.fs
@@ -3,10 +3,52 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D screenTexture;
+vec3 getSky(vec2 uv)
+{
+    float atmosphere = sqrt(1.0-uv.y);
+    vec3 skyColor = vec3(0.2,0.4,0.8);
+    
+    float scatter = pow(0.7,1.0 / 15.0);
+    scatter = 1.0 - clamp(scatter,0.8,1.0);
+    
+    vec3 scatterColor = mix(vec3(1.0),vec3(1.0,0.3,0.0) * 1.5,scatter);
+    return mix(skyColor,vec3(scatterColor),atmosphere / 1.3);
+    
+}
+
+vec3 getSun(vec2 uv){
+	float sun = 1.0 - distance(uv,vec2(0.7, 0.7));
+    sun = clamp(sun,0.0,1.0);
+    
+    float glow = sun;
+    glow = clamp(glow,0.0,1.0);
+    
+    sun = pow(sun,100.0);
+    sun *= 100.0;
+    sun = clamp(sun,0.0,1.0);
+    
+    glow = pow(glow,6.0) * 1.0;
+    glow = pow(glow,(uv.y));
+    glow = clamp(glow,0.0,1.0);
+    
+    sun *= pow(dot(uv.y, uv.y), 1.0 / 1.65);
+    
+    glow *= pow(dot(uv.y, uv.y), 1.0 / 2.0);
+    
+    sun += glow;
+    
+    vec3 sunColor = vec3(1.0,0.6,0.05) * sun;
+    
+    return vec3(sunColor);
+}
 
 void main()
 {
-    FragColor = vec4(mix(vec3(1,1,1), vec3(0.5,0.7,1), TexCoords.y), 1);
+    vec2 uv = TexCoords;
+
+    vec3 sky = getSky(uv);
+    vec3 sun = getSun(uv);
+
+    FragColor = = vec4(sky + sun,1.0);
 } 
 
